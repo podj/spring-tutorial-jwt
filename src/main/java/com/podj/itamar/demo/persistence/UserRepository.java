389fa1b02e197b008307bed90b049ce8b172d0e8
Itamar Podjarski <Itamar.podj@gmail.com>
2019-09-21 14:22:14 +0300

    Add the use of Optional class
    In order to be consistent with the Spring default method "findById" that return Optional, findByEmail should also return Optional.

--- src/main/java/com/podj/itamar/demo/persistence/UserRepository.java
+++ src/main/java/com/podj/itamar/demo/persistence/UserRepository.java
@@ -4,9 +4,11 @@ import com.podj.itamar.demo.domain.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-
-	User findByEmail(String email);
-
+	
+	Optional<User> findByEmail(String email);
+	
 }
